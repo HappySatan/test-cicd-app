9665a5c1914afb8651c10a7f59e397fa7366451a
HappySatan <eshwar.esarapu@blackx.io>
2022-03-16 18:20:21 +0530

    Extra chars

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>our mind blowing home page after PR.</h1>"""
+   text = """<h1>our mind blowing home page after PR + More PRRRRRRRRRRRRR.</h1>"""
    return HttpResponse(text)
